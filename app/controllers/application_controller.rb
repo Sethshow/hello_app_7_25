d7faef9b5eb26ffa7188838daf7b470ed2f95e9f
Ubuntu <ubuntu@ip-172-31-15-164.us-east-2.compute.internal>
2020-07-30 20:43:47 +0000

    Fix for route title cool_greeting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   def goodbye
     render html: "goodbye, world!"
   end
-  def cool greeting
+  def cool_greeting
     render html: "What's up?"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-root 'application#cool greeting'
+root 'application#cool_greeting'
 end
